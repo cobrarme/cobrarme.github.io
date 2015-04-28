07d6392f9115a55ddeec72e08a7c1c6174abd6d4
Breno Oliveira <breno26@gmail.com>
2015-04-28 19:20:07 -0300

    new texts

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
-    
+
     // Slimscroll
     $('.slimscroll').slimscroll({
         allowPageScroll: true
     });
-    
+
     // Wow
     new WOW().init();
-    
+
     // Smooth scroll
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
@@ -20,20 +20,20 @@ $( document ).ready(function() {
             'scrollTop': scrollTo
         }, 1000, 'easeInOutExpo');
     });
-    
-    $(window).scroll(function() {    
+
+    $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        
+
         if (scroll >= 60) {
             $(".navbar").addClass("whiteHeader");
         } else {
             $(".navbar").removeClass("whiteHeader");
         }
     });
-    
+
     // Tabs
     [].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
         new CBPFWTabs( el );
     });
-    
+
 });
