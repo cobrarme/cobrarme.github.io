a78993c18b1943f4ec62ca46fa169c2d457e490c
Breno Oliveira <breno26@gmail.com>
2015-10-25 10:47:17 -0200

    adiciona mixpanel

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -36,4 +36,22 @@ $( document ).ready(function() {
         new CBPFWTabs( el );
     });
 
+
+
+    $("#topmenu-home").click(track("Home"));
+    $("#topmenu-features").click(track("Features"));
+    $("#ftopmenu-preco").click(track("Preço"));
+    $("#topmenu-euquero").click(track("Eu quero menu superior"));
+    $("#topmenu-eu-quero-testar").click(track("Eu quero botão"));
+
+    $("#features-o-que-e").click(track("O que é?"));
+    $("#features-o-que-posso-fazer").click(track("O que posso fazer?"));
+    $("#features-como-posso-fazer").click(track("Como posso fazer?"));
+    $("#features-vantagens").click(track("Vantagens"));
+
+    $("#eu-quero").click(track("Envio dados pedindo"));
 });
+
+var track = function(key) {
+  mixpanel.track(key);
+}
